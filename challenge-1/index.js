a443516cc46a1c8d644b60eab1d96542c44fe0b1
Hasnaa <a02040456@gmail.com>
2026-04-29 10:30:43 +0300

    inject auth routers

--- challenge-1/index.js
+++ challenge-1/index.js
@@ -12,7 +12,7 @@ connectDb();
 const app = express();
 app.use(express.json());
 
-
+app.use('/api/auth' , require('./routes/auth.routes'));
 
 app.listen(3000 , ()=> {
     console.log("Server running on port 3000")
