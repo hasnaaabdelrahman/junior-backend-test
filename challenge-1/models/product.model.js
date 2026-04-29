0dd819a13c73406403a40235d69ed32ac16cd4d2
Hasnaa <a02040456@gmail.com>
2026-04-29 12:01:24 +0300

    products models

---
+++ challenge-1/models/product.model.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose')
+
+const productSchema = mongoose.Schema({
+      
+    name: {
+        type: String,
+        required: [true, 'Product name is required'],
+        trim: true
+
+    },
+    caregory: {
+        type: String,
+        required: false,
+        trim: true
+    },
+    price: {
+        type: Number,
+        required: [true, 'Price is required'],
+        min: [0.01, 'Price must be a positive number']
+    },
+    quantity: {
+        type: Number,
+        default: 1,
+        min: [0, 'Quantity cannot be negative'],
+        validate: {
+            validator: Number.isInteger,
+            message: 'Quantity must be a non-negative integer'
+        }
+    }
+}, {
+    timestamps: true
+}
+);
+
+module.exports = mongoose.model("Product" , productSchema);
