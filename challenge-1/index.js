83bacefcbbbb05d51c5eecfea679d730f16ac942
Hasnaa <a02040456@gmail.com>
2026-04-29 12:02:33 +0300

    inject products endpoints

--- challenge-1/index.js
+++ challenge-1/index.js
@@ -13,7 +13,9 @@ const app = express();
 app.use(express.json());
 
 app.use('/api/auth' , require('./routes/auth.routes'));
+app.use('/api/products' , require('./routes/product.routes'));
+
 
 app.listen(3000 , ()=> {
-    console.log("Server running on port 3000")
+    console.log("Server is running on port 3000")
 });
